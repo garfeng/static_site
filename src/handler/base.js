dba098e75928f7d7b0edf39316120c8982222b23
jry <jerrieyuan@hotmail.com>
2017-09-12 20:11:42 +0800

    delete all files

--- src/handler/base.js
+++
@@ -1,7 +0,0 @@
-
-let rand_index = 1;
-
-export const RandId = () => {
-  rand_index ++;
-  return rand_index;
-}
--- src/style.js
+++
@@ -1,4 +0,0 @@
-import '../static/css/bootstrap.css';
-import '../static/css/font-awesome.css';
-import '../static/css/github.css';
-import '../static/css/app.css';
